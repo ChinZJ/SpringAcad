74b4a26738b2b927eed2761e7b7412fa886a3506
ChinZJ <122326502+ChinZJ@users.noreply.github.com>
2025-12-30 14:32:24 +0800

    Add basic SoftwareEnginner class

---
+++ amigoscode/src/main/java/com/amigoscode/spring_boot/SoftwareEngineer.java
@@ -0,0 +1,57 @@
+package com.amigoscode.spring_boot;
+
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Long id;
+    private String name;
+    private String techStack;
+
+    // Allows for creation of object, then population using setters.
+    public SoftwareEngineer() {}
+
+    public SoftwareEngineer(Long id, String name, String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) 
+                && Objects.equals(name, that.name) 
+                && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
